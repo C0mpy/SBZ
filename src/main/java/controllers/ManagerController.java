9e8fb0dfe4c0aa3dd26ee25700300529ab2e202f
C0mpy <n.zunic@yahoo.com>
2017-07-03 17:02:00 +0200

    added some controllers

---
+++ src/main/java/controllers/ManagerController.java
@@ -0,0 +1,10 @@
+package app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/manager")
+public class ManagerController {
+
+}
